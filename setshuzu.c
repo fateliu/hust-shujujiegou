5b2da07d88ece22e889bc9999938b0ecd42d8fcc
liu <2979213874@qq.com>
2026-09-20 16:51:15 +0800

    加入contain 和clear

--- setshuzu.c
+++ setshuzu.c
@@ -6,3 +6,17 @@ typedef struct {
     char data[100][100];
     int size;
 } ArraySet;
+
+void initSet(ArraySet *set) {
+    set->size = 0;
+}   
+
+int contains(ArraySet *set, const char *element) {
+    for (int i = 0; i < set->size; i++) {
+        if (strcmp(set->data[i], element) == 0) {
+            return 1; // Element found
+        }
+    }
+    return 0; // Element not found
+} 
+
